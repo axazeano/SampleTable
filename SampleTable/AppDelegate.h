c0b3ea9b427c7a0723fd59c232c93e11695a1f08
–Т–ї–∞–і–Є–Љ–Є—А –Ъ—Г–±–∞–љ—Ж–µ–≤ <vladimirkubancev@mac-mini-ila.local>
2015-10-12 10:59:06 +0300

    Initial Commit

---
+++ SampleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleTable
+//
+//  Created by Владимир Кубанцев on 12.10.15.
+//  Copyright © 2015 Владимир Кубанцев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleTable
+//
+//  Created by Владимир Кубанцев on 12.10.15.
+//  Copyright © 2015 Владимир Кубанцев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
